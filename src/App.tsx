e249e1c12b4a4794cfdba8d863e43b7c5fbb1fe3
boulaffasae <boulaffasae@gmail.com>
2024-02-07 22:31:23 +0100

    Sharing my work

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,103 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { FormEvent, useEffect, useRef, useState } from "react";
+import "./App.css";
+
+function Time({ datetime }: { datetime: Date }) {
+  return (
+    <time dateTime={datetime.toISOString()}>{datetime.toLocaleString()}</time>
+  );
+}
+
+const timeouts: Record<number, number> = {};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const alarmsRef = useRef<number[]>([]);
+  const [date, setDate] = useState(new Date());
+  const [datetime, setDateTime] = useState(new Date());
+  const [alarms, setAlarms] = useState<number[]>([]);
+
+  useEffect(() => {
+    // THIS IS THE MAGIC PART
+    alarmsRef.current = alarms;
+  }, [alarms]);
+
+  function setAlarm(e: FormEvent) {
+    e.preventDefault();
+
+    const targetTime = datetime.getTime();
+
+    if (alarms.find((alarm) => alarm === targetTime)) return true;
+
+    setAlarms([...alarms, targetTime].sort((a, b) => a - b));
+
+    // Calculate the time difference between the current time and the target time
+    const currentTime = new Date();
+
+    const timeDiff = targetTime - currentTime.getTime();
+    console.log(targetTime, currentTime.getTime(), timeDiff);
+
+    timeouts[targetTime] = setTimeout(() => {
+      unsetAlarm(targetTime);
+
+      var alarmAudio = document.getElementById("alarm") as HTMLAudioElement;
+
+      if (alarmAudio) {
+        alarmAudio.play();
+      }
+    }, timeDiff);
+  }
+
+  function unsetAlarm(el: number) {
+    setAlarms(alarmsRef.current.filter((alarm) => alarm !== el));
+
+    if (timeouts[el]) {
+      clearTimeout(el);
+    }
+  }
+
+  function tick() {
+    setDate(new Date());
+  }
+
+  setInterval(tick, 1000);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <h1>The clock no-one thought of</h1>
+      <p>
+        <span>Time in local timezone: </span>
+        <strong>
+          <Time datetime={date} />
+        </strong>
       </p>
+
+      <form onSubmit={setAlarm}>
+        <input
+          type="datetime-local"
+          name="datetime"
+          onChange={(e) => setDateTime(new Date(e.target.value))}
+          min={date.toISOString().slice(0, -8)}
+          required
+        />
+        <input type="submit" value="+" />
+      </form>
+
+      <>
+        <h2>Alarms</h2>
+        {alarms.length ? (
+          <ul>
+            {alarms.map((alarm) => (
+              <li key={alarm}>
+                <Time datetime={new Date(alarm)} />
+                <button onClick={() => unsetAlarm(alarm)}>-</button>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>None</p>
+        )}
+      </>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
